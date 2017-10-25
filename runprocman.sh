a371629acd10214b5345c011cea20d3b402a2ae0
Andres Valenzuela <andres.valenzuela@tri.global>
2017-10-25 16:03:27 -0400

    Source spartan's setup_environment

--- runprocman.sh
+++ runprocman.sh
@@ -6,10 +6,13 @@ me=$(python -c 'import os; print(os.path.realpath("'"$0"'"))')
 MY_DIR=$(dirname $me)
 echo $MY_DIR
 . $MY_DIR/setup_environment.sh
-# . $SPARTAN_DIR/build/setup_environment.sh
+set +e
+. $SPARTAN_DIR/build/setup_environment.sh
+set -e
 export SPARTAN_SOURCE_DIR=/home/robot-lab/spartan-sammy
 export SPARTAN_BUILD_DIR=/home/robot-lab/spartan-sammy/build
 export SPARTAN_INSTALL_DIR=/home/robot-lab/spartan-sammy/build/install
+export DRAKE_RESOURCE_ROOT=$DRAKE_DIR
 
 cd $DRAKE_DIR
 
